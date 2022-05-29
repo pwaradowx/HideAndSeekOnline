bf05ce214dfb9efa38e096fb34ab07d92e39e679
Abbibbas <76590685+Abbibbas@users.noreply.github.com>
2022-05-29 15:52:38 +0300

    [feat] Hit hider effect for seeker
    Added visual effect to seeker's crosshair when he hits the hider.

--- HideAndSeekOnline/Assets/Scripts/Game/Player/Seeker.cs
+++ HideAndSeekOnline/Assets/Scripts/Game/Player/Seeker.cs
@@ -7,12 +7,17 @@ namespace Project.Game.Player
     public class Seeker : NetworkBehaviour
     {
         [SerializeField] private Camera cam;
+        [SerializeField] private RectTransform hitEffect;
         
         private PlayerInput _playerInput;
 
         private bool _shouldFire;
         private float _damage = 0.75f;
         private const float FireDistance = 100f;
+
+        private bool _hiderGotHit;
+        private readonly Vector3 _effectSmallScale = new Vector3(1f, 1f, 1f);
+        private readonly Vector3 _effectBigScale = new Vector3(1.1f, 1.1f, 1.1f);
         
         public override void OnNetworkSpawn()
         {
@@ -21,7 +26,14 @@ namespace Project.Game.Player
             _playerInput = GetComponent<PlayerInput>();
 
             _playerInput.actions["Fire"].started += callback => _shouldFire = true;
-            _playerInput.actions["Fire"].canceled += callback => _shouldFire = false;
+            _playerInput.actions["Fire"].canceled += callback =>
+            {
+                _shouldFire = false;
+                hitEffect.localScale = _effectSmallScale;
+                hitEffect.gameObject.SetActive(false);
+            };
+            hitEffect.localScale = _effectSmallScale;
+            hitEffect.gameObject.SetActive(false);
         }
 
         private void FixedUpdate()
@@ -31,15 +43,37 @@ namespace Project.Game.Player
             if (!_shouldFire) return;
             
             HandleFire();
+            HandleHitEffect();
         }
 
         private void HandleFire()
         {
             if (Physics.Raycast(cam.transform.position, cam.transform.forward, out var hit, FireDistance))
             {
-                if (!hit.collider.TryGetComponent(out Hider hider)) return;
+                _hiderGotHit = hit.collider.TryGetComponent(out Hider hider);
+                if (!_hiderGotHit) return;
                 
                 InflictDamageServerRpc(hider.NetworkObjectId);
+            }
+        }
+
+        private void HandleHitEffect()
+        {
+            if (_hiderGotHit)
+            {
+                if (!hitEffect.gameObject.activeSelf)
+                {
+                    hitEffect.gameObject.SetActive(true);
+                }
+
+                hitEffect.localScale = hitEffect.localScale == _effectSmallScale ? _effectBigScale : _effectSmallScale;
+            }
+            else
+            {
+                if (hitEffect.gameObject.activeSelf)
+                {
+                    hitEffect.gameObject.SetActive(false);
+                }
             }
         }
 
